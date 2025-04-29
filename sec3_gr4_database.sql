320dc3610c339fdd70af23996387b31693884ba7
Chanikarntamp <tampja2548@gmail.com>
2025-04-29 11:51:36 +0700

    Update sec3_gr4_database.sql

--- sec3_gr4_database.sql
+++ sec3_gr4_database.sql
@@ -31,12 +31,15 @@ CREATE TABLE IF NOT EXISTS Product (
     Product_Img LONGTEXT 
 );
 
-CREATE TABLE `sessions` (
-  `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `expires` int unsigned NOT NULL,
-  `data` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
-  PRIMARY KEY (`session_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- Table: sessions (for express-mysql-session)
+CREATE TABLE IF NOT EXISTS sessions (
+    session_id VARCHAR(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+    expires INT UNSIGNED NOT NULL,
+    data MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+    PRIMARY KEY (session_id)
+) ENGINE=InnoDB
+  DEFAULT CHARSET=utf8mb4
+  COLLATE=utf8mb4_0900_ai_ci;NGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO Admin_Info (Admin_ID, First_Name, Last_Name, Address, Dateofbirth, Email) VALUES
 ('AD66047', 'Thanyathip', 'Korapintavangkul', '16 Chotisahai, rimklongprapa road, Bangsue, Bangkok 10800', '2005-09-24', 'thanyathip.kor@student.mahidol.ac.th'),
